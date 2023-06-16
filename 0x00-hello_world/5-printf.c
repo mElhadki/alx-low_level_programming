9d041c3c24653caa08498b0f5f942666cd4648c1
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-06-16 01:41:25 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-
+#include <stdio.h>
+/**
+ *main - Prints a sentence with proper grammar followed by a new line
+ *Return: 0 if there are no errors, non-zero if there are errors.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
