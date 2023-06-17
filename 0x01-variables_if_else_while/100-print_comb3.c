b3cd30fa9078da51f67a628a57de2d274be3b14d
mElhadki <elhadkimariem3@gmail.com>
2023-06-16 18:18:39 -0700

    done

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int tensDigit;
+int onesDigit;
+for (tensDigit = 0; tensDigit <= 8; tensDigit++)
+{
+for (onesDigit = tensDigit + 1; onesDigit <= 9; onesDigit++)
+{
+putchar(tensDigit + '0');
+putchar(onesDigit + '0');
+if (tensDigit != 8 || onesDigit != 9)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
