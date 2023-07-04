c170373fd48090e39adff2705fff344ea6b2b2d9
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-07-04 02:44:50 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strstr - Locates a substring.
  * @haystack: The string to be searched.
@@ -9,32 +8,24 @@
  *                                       of the located substring.
  *         If the substring is not located - NULL.
  */
-
 char *_strstr(char *haystack, char *needle)
 {
-	int index;
-
-	if (*needle == 0)
-		return (haystack);
-
-	while (*haystack)
-	{
-		index = 0;
-
-		if (haystack[index] == needle[index])
-		{
-			do {
-
-				if (needle[index + 1] == '\0')
-					return (haystack);
-
-				index++;
-
-			} while (haystack[index] == needle[index]);
-		}
-
-		haystack++;
-	}
-
-	return ('\0');
+int index;
+if (*needle == 0)
+return (haystack);
+while (*haystack)
+{
+index = 0;
+if (haystack[index] == needle[index])
+{			
+do {				
+if (needle[index + 1] == '\0')
+return (haystack);
+index++;
+}
+while (haystack[index] == needle[index]);
+}
+haystack++;
+}
+return ('\0');
 }
