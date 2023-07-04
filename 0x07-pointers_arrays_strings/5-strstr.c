f3e534a353f3390e3a166e516431baf43b82d78c
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-07-04 03:32:54 +0100

    Update 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,29 +1,41 @@
 #include "main.h"
 /**
-* _strstr - Locates a substring.
-* @haystack: The string to be searched.
-* @needle: The substring to be located.
-* Return: If the substring is located - a pointer to the beginningof the located substring.
-* If the substring is not located - NULL.
+* _strncmp - compares two strings within b bytes.
+* @s1: string to check
+* @s2: string to check from
+* @b: number of bytes to check
+* Return: 0 if strings are the same
+* any other number if they are different.
+*/
+int _strncmp(char *s1, char *s2, unsigned int b)
+{
+unsigned int i;
+for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
+{
+s1++;
+s2++;
+}
+return (i - b);
+}
+/**
+* _strstr - locates a substring
+* @haystack: string to look though
+* @needle: substring to find
+* Return: pointer to string with result of search
 */
 char *_strstr(char *haystack, char *needle)
 {
-int index;
-if (*needle == 0)
-return (haystack);
-while (*haystack)
+unsigned int len;
+/*Get length of needle for strncmp*/
+len = 0;
+while (needle[len] != '\0')
+len++;
+/*compare substring*/
+while (*haystack != '\0')
 {
-index = 0;
-if (haystack[index] == needle[index])
-{
-do {				
-if (needle[index + 1] == '\0')
+if (_strncmp(haystack, needle, len) == 0)
 return (haystack);
-index++;
-}
-while (haystack[index] == needle[index]);
-}
 haystack++;
 }
-return ('\0');
+return (NULL);
 }
