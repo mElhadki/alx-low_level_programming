2fdd929627c261d70e9e57c971d4ae7489299159
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-07-04 02:37:24 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _strchr - Locates a character in a string.
  * @s: The string to be searched.
@@ -10,13 +9,11 @@
  */
 char *_strchr(char *s, char c)
 {
-	int index;
-
-	for (index = 0; s[index] >= '\0'; index++)
-	{
-		if (s[index] == c)
-			return (s + index);
-	}
-
-	return ('\0');
+int index;
+for (index = 0; s[index] >= '\0'; index++)
+{
+if (s[index] == c)
+return (s + index);
+}
+return ('\0');
 }
