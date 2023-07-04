9e2cd097d2cfc2d5feaf206e3540f2cc4ba699f4
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-07-04 03:26:29 +0100

    Update 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,41 @@
-#include "main.h"
-#include <stdio.h>
+#include "holberton.h" 
 /**
-* print_diagsums - Prints the sum of the two diagonals of a square matrix.
-* @a: Pointer to the square matrix of integers.
-* @size: Size of the matrix.
-*/
-void print_diagsums(int *a, int size)
+ * _strncmp - compares two strings within b bytes.
+ * @s1: string to check
+ * @s2: string to check from
+ * @b: number of bytes to check
+ * Return: 0 if strings are the same
+ * any other number if they are different.
+ */
+int _strncmp(char *s1, char *s2, unsigned int b)
 {
-int i, sum1 = 0, sum2 = 0;
-for (i = 0; i < size; i++)
+unsigned int i;
+for (i = 0; i <= b && *s1 == *s2 && *s1 != '\0' && *s2 != '\0'; i++)
 {
-sum1 += a[i * size + i]; /* Elements in the main diagonal */
-sum2 += a[i * size + (size - 1 - i)]; /* Elements in the secondary diagonal */
+s1++;
+s2++;
 }
-printf("%d, %d\n", sum1, sum2);
+return (i - b);
+}
+/**
+* _strstr - locates a substring
+* @haystack: string to look though
+* @needle: substring to find
+* Return: pointer to string with result of search
+*/
+char *_strstr(char *haystack, char *needle)
+{
+unsigned int len;
+/*Get length of needle for strncmp*/
+len = 0;
+while (needle[len] != '\0')
+len++;
+/*compare substring*/
+while (*haystack != '\0')
+{
+if (_strncmp(haystack, needle, len) == 0)
+return (haystack);
+haystack++;
+}
+return (NULL);
 }
