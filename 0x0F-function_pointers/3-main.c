e624ad9b8c6534c00428139dbe59b53189750007
ELHADKI MARIEM <57253112+mElhadki@users.noreply.github.com>
2023-08-01 02:50:36 +0100

    Update 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,48 +1,37 @@
-#include "3-calc.h"
 #include <stdio.h>
 #include <stdlib.h>
-/**
- * main - get_op_func has operators correlated with
- * func signs and funcs from op_func
- * if not 4 arguments, return Error & exit 98
- * if op is null, return Error & exit 99
- * if div or mod 0, return Error & exit 100
- * run calc, input one, operator, input two = pointer res to get_op
- * @argc: arguments
- * @argv: double pointer to arguments
- * Return: 0
- */
+#include "3-calc.h"
+#include <stddef.h>
+
 int main(int argc, char *argv[])
 {
-	int one, two, ans;
-	int (*res)(int, int);
-	char *get_op;
+    int num1, num2, result;
+    int (*operator)(int, int);
 
-	if (argc != 4)
-	{
-		printf("Error\n");
-		exit(98);
-	}
+    if (argc != 4)
+    {
+        printf("Error\n");
+        return (98);
+    }
 
-	one = atoi(argv[1]);
-	two = atoi(argv[3]);
-	get_op = argv[2];
+    num1 = atoi(argv[1]);
+    num2 = atoi(argv[3]);
+    operator = get_op_func(argv[2]);
 
-	if (get_op_func(argv[2]) == NULL || argv[2][1] != '\0')
-	{
-		printf("Error\n");
-		exit(99);
-	}
+    if (operator == NULL || argv[2][1] != '\0')
+    {
+        printf("Error\n");
+        return (99);
+    }
 
-	if ((*get_op == '/' || *get_op == '%') && (*argv[3] == '0'))
-	{
-		printf("Error\n");
-		exit(100);
-	}
+    if ((*argv[2] == '/' || *argv[2] == '%') && num2 == 0)
+    {
+        printf("Error\n");
+        return (100);
+    }
 
-	res = get_op_func(get_op);
-	ans = res(one, two);
+    result = operator(num1, num2);
+    printf("%d\n", result);
 
-	printf("%d\n", ans);
-	return (0);
+    return (0);
 }
