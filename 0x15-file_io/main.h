72acd32bfdb805aacc84b9825875d352b2712f24
melhadki <elhadkimariem3@gmail.com>
2023-10-30 13:15:05 -0700

    ok

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,4 +1,4 @@
-[200~#ifndef FILE_IO
+#ifndef FILE_IO
 #define FILE_IO
 
 #include <sys/types.h>
@@ -12,3 +12,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif
+
